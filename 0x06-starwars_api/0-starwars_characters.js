34e4dd98a95be10f571a9a30b2eb97bcc604c2ad
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-09-05 16:17:56 -0700

    task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,31 @@
 #!/usr/bin/node
+// Prints all characters of a Star Wars movie using the ALX Star Wars API
+
+if (process.argv.length !== 3) {
+  process.exit();
+}
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 const request = require('request');
-const movieId = process.argv[2];
-const options = {
-  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
-  method: 'GET'
-};
 
-request(options, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+function printNextCharacter (urls) {
+  const characterURL = urls.shift();
+  if (characterURL) {
+    request(characterURL, function (error, response, body) {
+      if (!error) {
+        console.log(JSON.parse(body).name);
+        printNextCharacter(urls);
       }
+    });
+  }
+}
+
+function printStarWarsCharacters (url) {
+  request(url, function (error, response, body) {
+    if (!error) {
+      printNextCharacter(JSON.parse(body).characters);
     }
   });
 }
+
+printStarWarsCharacters(url);
